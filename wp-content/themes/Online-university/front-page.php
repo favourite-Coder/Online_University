93800cd00785eaa557c657988db1f5ae1eade115
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-10 05:28:51 +0100

    Update front-page.php
    Custom Queries

--- wp-content/themes/Online-university/front-page.php
+++ wp-content/themes/Online-university/front-page.php
@@ -46,29 +46,30 @@ get_header();
     <div class="full-width-split__two">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+              
+    <!-- Blog Post Summary -->
+    <!-- Custom Query to fetch 2 post -->
+                  <?php
+         $homePagePosts = new WP_Query(array(
+             'posts_per_page' => 2
+         ));
 
-            <!-- First Blog Post Summary -->
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="#">
-                    <span class="event-summary__month">Jan</span>
-                    <span class="event-summary__day">20</span>
+         while ($homePagePosts->have_posts()) {
+            $homePagePosts->the_post(); ?>
+                         <div class="event-summary">
+                <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month"><?php the_time('M'); ?></span>
+                    <span class="event-summary__day"><?php the_time('d'); ?></span>
                 </a>
                 <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
-                    <p>For the 100th year in a row, we were voted #1. <a href="#" class="nu gray">Read more</a></p>
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" 
+                    class="nu gray">Read more</a></p>
                 </div>
             </div>
-            <!-- Second Blog Post Summary -->
-            <div class="event-summary">
-                <a class="event-summary__date event-summary__date--beige t-center" href="#">
-                    <span class="event-summary__month">Feb</span>
-                    <span class="event-summary__day">04</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-                    <p>Two of our professors have been in the national news lately. <a href="#" class="nu gray">Read more</a></p>
-                </div>
-            </div>
+         <?php } wp_reset_postdata();
+          ?>
+
 
             <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
