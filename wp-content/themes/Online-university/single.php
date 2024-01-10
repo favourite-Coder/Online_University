5c5173ab3629245aa3f41a216dafe0a7614c6ded
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-10 01:38:27 +0100

    Blog Archive

--- wp-content/themes/Online-university/single.php
+++ wp-content/themes/Online-university/single.php
@@ -2,8 +2,37 @@
 
 while (have_posts()) {
  the_post(); ?>
- <h2><?php the_title(); ?></h2>
-<?php  the_content(); ?>
+
+<div class="page-banner">
+        <!-- Background image -->
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
+        <!-- Page banner content -->
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p>DON'T FORGET TO REPLACE ME LATER</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+    <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                    <!-- Link back to the parent page -->
+                    <a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>">
+                        <i class="fa fa-home" aria-hidden="true"></i> 
+                         Blog Home
+                    </a>
+                    <!-- Display current page title -->
+                    <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('M/d/Y/ - h:Ha'); ?> 
+    <?php echo get_the_category_list(', '); ?>
+  </span>
+                </p>
+            </div>
+
+    <div class="generi-content"><?php the_content(); ?></div>
+    </div>
+
  <?php }
 
   get_footer();
