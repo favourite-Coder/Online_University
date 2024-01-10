c7680d29baa4135d3694b1164793e11349aba651
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-10 16:19:51 +0100

    Manipulating Default URL Based Queries

--- wp-content/themes/Online-university/front-page.php
+++ wp-content/themes/Online-university/front-page.php
@@ -23,7 +23,7 @@ get_header();
             <?php
             $today = date('Ymd');
             $homePageEvents = new WP_Query(array(
-                'posts_per_page' => -1,
+                'posts_per_page' => 2,
                 'post_type' => 'event',
                 'meta_key' => 'event_date',
                 'orderby' => 'meta_value_num',
