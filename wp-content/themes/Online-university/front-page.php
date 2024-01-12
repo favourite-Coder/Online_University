83a3d073b95269c99553f317f65291643d984916
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-12 17:46:47 +0100

    get_template_part()
    Reduce Duplication - "get_template_part()

--- wp-content/themes/Online-university/front-page.php
+++ wp-content/themes/Online-university/front-page.php
@@ -39,32 +39,10 @@ get_header();
             ));
 
             while ($homePageEvents->have_posts()) {
-                $homePageEvents->the_post(); ?>
+                $homePageEvents->the_post(); 
 
-                <div class="event-summary">
-                    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                        <span class="event-summary__month">
-                            <?php
-                            // Retrieve the event date for each event
-                            $eventDate = new DateTime(get_field('event_date'));
-                            ?>
-
-                        </span>
-                        <span class="event-summary__month"><?php echo $eventDate->format('M'); ?></span>
-                        <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-                    </a>
-                    <div class="event-summary__content">
-                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?php if (has_excerpt()) {
-                                echo get_the_excerpt();
-                            } else {
-                                echo wp_trim_words(get_the_content(), 15);
-                            }
-                            ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                    </div>
-                </div>
-
-            <?php }
+                get_template_part('template-parts/content-event');
+          }
             wp_reset_postdata();
             ?>
 
@@ -83,23 +61,11 @@ get_header();
             ));
 
             while ($homePagePosts->have_posts()) {
-                $homePagePosts->the_post(); ?>
-                <div class="event-summary">
-                    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-                        <span class="event-summary__month"><?php the_time('M'); ?></span>
-                        <span class="event-summary__day"><?php the_time('d'); ?></span>
-                    </a>
-                    <div class="event-summary__content">
-                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?php if (has_excerpt()) {
-                                echo get_the_excerpt();
-                            } else {
-                                echo wp_trim_words(get_the_content(), 18);
-                            }
-                            ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
-                    </div>
-                </div>
-            <?php }
+                $homePagePosts->the_post();
+
+                get_template_part('template-parts/content-blog');
+
+               }
             wp_reset_postdata();
             ?>
 
