502315aa4aea57ce23110e8347f5025ea2654686
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-14 20:05:05 +0100

    Update search-route.php
    Working With Multiple Post Types

--- wp-content/themes/Online-university/inc/search-route.php
+++ wp-content/themes/Online-university/inc/search-route.php
@@ -1,28 +1,89 @@
-<?php 
+<?php
 
 add_action('rest_api_init', 'universityRegisterSearch');
 
-function universityRegisterSearch () {
-    register_rest_route('university/v1', 'search',array(
-      'methods' => WP_REST_SERVER::READABLE, 
-      'callback' => 'universitySearchResults'
+function universityRegisterSearch()
+{
+    register_rest_route('university/v1', 'search', array(
+        'methods' => WP_REST_SERVER::READABLE,
+        'callback' => 'universitySearchResults'
     ));
 };
 
-function universitySearchResults () {
-        $professors = new WP_Query(array(
-          'post_type' => 'professor'
-        ));
-$professorResults = array ();
+function universitySearchResults($data)
+{
+    $mainQuery = new WP_Query(array(
+        'post_type' => array('post', 'page', 'professor', 'program', 'event'),
+        's' => sanitize_text_field($data['term']) 
 
-while ($professors->have_posts()) {
-    $professors->the_post();
-    array_push($professorResults, array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink(),
-        'type' => get_the_content(),
     ));
-}
+    $results = array(
+        'generalInfo' => array() ,
+        'professors' =>  array() ,
+        'programs' =>  array()  ,
+        'events' => array()
+    );
 
-  return $professorResults;
+    while ($mainQuery->have_posts()) {
+        $mainQuery->the_post();
+
+        //generalInfo
+
+          if (get_post_type() == 'post' OR get_post_type() == 'page') {
+           
+            array_push($results['generalInfo'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+          
+        } 
+
+        //professors
+
+        if (get_post_type() == 'professor') {
+           
+            array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink()
+            ));
+          
+        } 
+
+                //programs
+
+                if (get_post_type() == 'program') {
+           
+                    array_push($results['programs'], array(
+                        'title' => get_the_title(),
+                        'permalink' => get_the_permalink()
+                    ));
+                }        
+
+                //events
+
+                if (get_post_type() == 'event') {
+           
+                    array_push($results['events'], array(
+                        'title' => get_the_title(),
+                        'permalink' => get_the_permalink()
+                    ));
+                }                      
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+    return $results;
 };
