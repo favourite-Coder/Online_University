8da6b65dfc6cbc14003f21257d7a5e2c152b55d9
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-14 19:25:16 +0100

    Update search-route.php
    Created personal RAW JSON data

--- wp-content/themes/Online-university/inc/search-route.php
+++ wp-content/themes/Online-university/inc/search-route.php
@@ -10,5 +10,19 @@ function universityRegisterSearch () {
 };
 
 function universitySearchResults () {
-    return 'Developer in the making';
+        $professors = new WP_Query(array(
+          'post_type' => 'professor'
+        ));
+$professorResults = array ();
+
+while ($professors->have_posts()) {
+    $professors->the_post();
+    array_push($professorResults, array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+        'type' => get_the_content(),
+    ));
+}
+
+  return $professorResults;
 };
